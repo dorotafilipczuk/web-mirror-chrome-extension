c995dace8901bc73694c0bf593df81d4bf5b2574
Dorota <dorota@Dorotas-MacBook-Pro.local>
2017-07-03 16:33:51 +0100

    Detecting browser extensions works

--- detect.js
+++ detect.js
@@ -1,11 +1,21 @@
-var detect = function(base, if_installed, if_not_installed) {
-    var s = document.createElement('script');
-    s.onerror = if_not_installed;
-    s.onload = if_installed;
-    document.body.appendChild(s);
-    s.src = base + '/manifest.json';
+var ghosteryExtId = 'mlomiejdfkolichcflejclcbmpeaniij';
+var adBlockPlusExtId = 'cfhdojbkjhnklbpkdaibdccddilifddb';
+var disconnectExtId = 'jeoacafpbcihiomhlakheieifhpjdfeo';
+var uBlockOriginExtId = 'cjpalhdlnbpafiamejdnhcphjbkeiagm';
+
+function detectExt(extensionId) {
+	chrome.management.get(extensionId, function(extensionInfo) {
+    	var isInstalled;
+    	if (chrome.runtime.lastError) {
+    	  	//When the extension does not exist, an error is generated
+    	  	isInstalled = false;
+			alert('not installed!');
+    	} else {
+     	 	//The extension is installed. Use "extensionInfo" to get more details
+      		isInstalled = true;
+      		alert('installed!');
+    	}
+	});
 }
-// detect Ghostery
-detect('chrome-extension://mlomiejdfkolichcflejclcbmpeaniij', 
-    function() {alert('installed!');},
-    function() {alert('not installed!');});
+
+detectExt(ghosteryExtId);
