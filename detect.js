d448d1530cb949c13f8d84e62424d9c9e1dee25e
dorotafilipczuk <dorota.filipczuk@gmail.com>
2017-10-11 17:10:42 +0100

    Set interval for the detect function to run every 1h

--- detect.js
+++ detect.js
@@ -7,15 +7,30 @@ function detectExt(extensionId) {
 	chrome.management.get(extensionId, function(extensionInfo) {
     	var isInstalled;
     	if (chrome.runtime.lastError) {
-    	  	//When the extension does not exist, an error is generated
+    	  	// When the extension does not exist, an error is generated
     	  	isInstalled = false;
-			alert('not installed!');
+			    alert('not installed!');
     	} else {
-     	 	//The extension is installed. Use "extensionInfo" to get more details
+     	 	// The extension is installed. Use "extensionInfo" to get more details
       		isInstalled = true;
       		alert('installed!');
     	}
+      // TODO(dorotafilipczuk): Log the value of isInstalled and the timestamp
+      // in the database.
 	});
 }
 
-detectExt(ghosteryExtId);
+function detect() {
+    detectExt(ghosteryExtId);
+    detectExt(adBlockPlusExtId);
+    detectExt(disconnectExtId);
+    detectExt(uBlockOriginExtId);
+
+    // TODO(dorotafilipczuk): If the first timestamp in the db + 30 days is
+    // smaller than the current time stamp, then stop; redirect the user to
+    // the exit survey and notify the admin.
+
+    // TODO(dorotafilipczuk): interventions / notifications.
+}
+
+setInterval(detect, 60 * 60 * 1000);
